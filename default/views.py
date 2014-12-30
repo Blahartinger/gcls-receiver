024113a32f3059241fd5a06c817ca2d94546f239
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2014-12-30 01:39:04 +0100

    fix

--- default/views.py
+++ default/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests, requests.utils
 
 
 # Create your views here.
 def corsproxy(request, requestedurl):
-    return HttpResponse(requestedurl)
+
+	proxy_response = requests.get(requestedurl)
+
+    response = HttpResponse(proxy_response.body, content_type=proxy_response.headers['content-type'])
+    response.status_code = status_code
+    response["Access-Control-Allow-Origin"] = "https://gcls.herokuapp.com"
+    response["Access-Control-Allow-Credentials"] = "true"
+    return response
