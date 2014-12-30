d0886509525052bce6804387cfe0039c34bc629d
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2014-12-30 01:51:25 +0100

    fix

--- default/views.py
+++ default/views.py
@@ -9,7 +9,7 @@ def corsproxy(request, requestedurl):
     print(requestedurl)
     proxy_response = requests.get(requestedurl)
 
-    response = HttpResponse(proxy_response.body, content_type=proxy_response.headers['content-type'])
+    response = HttpResponse(proxy_response.content, content_type=proxy_response.headers['content-type'])
     response.status_code = proxy_response.status_code
     response["Access-Control-Allow-Origin"] = "https://gcls.herokuapp.com"
     response["Access-Control-Allow-Credentials"] = "true"
